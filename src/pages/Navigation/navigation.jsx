92e74d1ac680aa6a34388b7320a60dd5caaf9d2b
tierney03 <tierney_03@mac.com>
2023-03-29 14:58:31 -0500

    Adding Dependencies and adding a navbar for SPA. Added gitignore

--- src/pages/Navigation/navigation.jsx
+++ src/pages/Navigation/navigation.jsx
@@ -1,3 +1,17 @@
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+
 export default function Navigation() {
-  return <h1>Navbar</h1>;
+  return (
+    <Navbar bg="light" variant="light">
+      <Container>
+        <Navbar.Brand href="/">Project</Navbar.Brand>
+        <Nav className="me-auto">
+          <Nav.Link href="/quiz">Quiz</Nav.Link>
+          <Nav.Link href="/results">Results</Nav.Link>
+        </Nav>
+      </Container>
+    </Navbar>
+  );
 }
